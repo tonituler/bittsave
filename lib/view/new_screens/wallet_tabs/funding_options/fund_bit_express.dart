12e757b6f30a6ecdb41aa5c31b8bf99bf7cb8226
Kingah12 <54068471+Kingah12@users.noreply.github.com>
2022-06-22 18:20:39 -0700

    conflict resolved

---
+++ lib/view/new_screens/wallet_tabs/funding_options/fund_bit_express.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import '../funding_usd_wallet_page.dart';
+
+const kLightTextStyle = TextStyle(
+    color: Colors.pinkAccent, fontSize: 20, fontWeight: FontWeight.w300);
+
+class FundBitExpress extends StatefulWidget {
+  const FundBitExpress({Key key}) : super(key: key);
+
+  @override
+  State<FundBitExpress> createState() => _FundBitExpressState();
+}
+
+class _FundBitExpressState extends State<FundBitExpress> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0, top: 10),
+                child: BackButtons(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, top: 30, bottom: 10),
+                child: BoldTextTitle(
+                  data: 'How much do you want to\ndeposit?',
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, top: 3.0, bottom: 10),
+                child: LightText(text: 'Exchange rate: 1USD = 415.0 NGN'),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 45,
+                  bottom: 10,
+                ),
+                height: 200,
+                width: double.maxFinite,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '\$0.00',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 70),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: LightText(text: 'USD Bal: \$500.00'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 250,
+                margin: const EdgeInsets.only(top: 50.0, right: 12),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.black),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InnerContainer(
+                          height: 100,
+                          col: Colors.white,
+                          data: 5,
+                          widget: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "you'll pay",
+                                style: kLightTextStyle.copyWith(
+                                    color: Colors.grey),
+                              ),
+                              Text(
+                                'N40,000.00',
+                                style: kLightTextStyle.copyWith(
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                          style: kLightTextStyle),
+                      InnerContainer(
+                          height: 70,
+                          col: Colors.pink,
+                          data: 13,
+                          widget: Text('Continue',
+                              style: kLightTextStyle.copyWith(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w300))),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget LightText({String text}) {
+    return Text(
+      text,
+      style: kLightTextStyle,
+    );
+  }
+
+  Widget InnerContainer(
+      {double height,
+      Color col,
+      double data,
+      String text,
+      TextStyle style,
+      Widget widget}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: height,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: col, borderRadius: BorderRadius.circular(data)),
+        child: Center(
+          child: widget,
+        ),
+      ),
+    );
+  }
+}
--- lib/view/new_screens/wallet_tabs/funding_usd_wallet_page.dart
+++ lib/view/new_screens/wallet_tabs/funding_usd_wallet_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:six_cash/util/color_resources.dart';
+import 'package:six_cash/view/new_screens/wallet_tabs/funding_options/fund_bit_express.dart';
 
 class FundingUsdWallet extends StatefulWidget {
   const FundingUsdWallet({Key key}) : super(key: key);
@@ -13,114 +14,142 @@ class _FundingUsdWalletState extends State<FundingUsdWallet> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16.0, top: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.arrow_back_ios_outlined,
-                  color: Colors.pink,
-                  size: 28,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BackButtons(),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 10.0, top: 20, left: 8),
+                  child: BoldTextTitle(data: 'Funding USD Wallet'),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0, top: 20, left: 8),
-                child: Text(
-                  'Funding USD Wallet',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 30),
+                // SizedBox(),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, bottom: 40),
+                  child: Text(
+                    'We have rebranding the methods of depositing USD into your wallet.',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 22),
+                  ),
                 ),
-              ),
-              // SizedBox(),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, bottom: 40),
-                child: Text(
-                  'We have rebranding the methods of depositing USD into your wallet.',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w300,
-                      fontSize: 22),
-                ),
-              ),
-              Padding(
-                padding:
-                    EdgeInsets.only(top: 10, right: 20, left: 5, bottom: 80),
-                child: PhysicalModel(
-                  elevation: 8,
-                  color: Colors.pink,
-                  shadowColor: Colors.lightBlueAccent,
-                  borderRadius: BorderRadius.circular(30),
-                  child: Container(
-                    padding: EdgeInsets.all(14),
-                    height: 200,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              'USD',
+                Padding(
+                  padding:
+                      EdgeInsets.only(top: 10, right: 20, left: 5, bottom: 80),
+                  child: PhysicalModel(
+                    elevation: 8,
+                    color: Colors.pink,
+                    shadowColor: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(30),
+                    child: Container(
+                      padding: EdgeInsets.all(14),
+                      height: 200,
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'USD',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.w200),
+                              ),
+                              Spacer(),
+                              Text(
+                                '\$',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.w300),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Text(
+                              '\$500.00',
                               style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 35,
-                                  fontWeight: FontWeight.w200),
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 40),
                             ),
-                            Spacer(),
-                            Text(
-                              '\$',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 50,
-                                  fontWeight: FontWeight.w300),
-                            )
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10.0),
-                          child: Text(
-                            '\$500.00',
+                          ),
+                          Text(
+                            'Your Balance',
                             style: TextStyle(
                                 color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 40),
-                          ),
-                        ),
-                        Text(
-                          'Your Balance',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w200,
-                              fontSize: 25),
-                        )
-                      ],
+                                fontWeight: FontWeight.w200,
+                                fontSize: 25),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              getFunds(
-                title: "BitXpress",
-                subTitle:
-                    'Fund your USD wallet using our \nexternal partners and assoiciates',
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              getFunds(
-                title: 'Request from a friend ',
-                subTitle:
-                    'Fund your USD wallet by requesting\nfund from bitsave users.',
-              )
-            ],
+                getFunds(
+                  title: "BitXpress",
+                  subTitle:
+                      'Fund your USD wallet using our \nexternal partners and assoiciates',
+                  ontap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const FundBitExpress();
+                    }));
+                  },
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                getFunds(
+                  title: 'Request from a friend ',
+                  subTitle:
+                      'Fund your USD wallet by requesting\nfund from bitsave users.',
+                )
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BoldTextTitle extends StatelessWidget {
+  final String data;
+  const BoldTextTitle({Key key, this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: TextStyle(
+          color: Colors.black, fontWeight: FontWeight.w600, fontSize: 30),
+    );
+  }
+}
+
+class BackButtons extends StatelessWidget {
+  const BackButtons({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Icon(
+        Icons.arrow_back_ios_outlined,
+        color: Colors.pink,
+        size: 28,
       ),
     );
   }
@@ -129,56 +158,61 @@ class _FundingUsdWalletState extends State<FundingUsdWallet> {
 class getFunds extends StatelessWidget {
   final String title;
   final String subTitle;
-  const getFunds({this.subTitle, this.title});
+  final Function ontap;
+  const getFunds({this.subTitle, this.title, this.ontap});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 10.0, right: 10),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20.0),
-            child: Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                  color: Colors.pink, borderRadius: BorderRadius.circular(16)),
+    return GestureDetector(
+      onTap: ontap,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10.0, right: 10),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(16)),
+                child: Icon(
+                  Icons.qr_code,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  subTitle,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 20,
+                      color: Colors.grey),
+                )
+              ],
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
               child: Icon(
-                Icons.qr_code,
-                color: Colors.white,
-                size: 40,
+                Icons.arrow_forward_ios_outlined,
+                color: Colors.grey,
+                size: 28,
               ),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-              ),
-              SizedBox(height: 8),
-              Text(
-                subTitle,
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 20,
-                    color: Colors.grey),
-              )
-            ],
-          ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: Icon(
-              Icons.arrow_forward_ios_outlined,
-              color: Colors.grey,
-              size: 28,
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
